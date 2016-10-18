b6e742662c81cebafa18622f06a04dec693df541
mohebifar <netfars@gmail.com>
2016-10-18 13:43:50 -0230

    chore(PbsCommands): Rename PbsCommand to PbsCommands

--- pbs_generator/__init__.py
+++ pbs_generator/__init__.py
@@ -2,3 +2,4 @@ from Pbs import Pbs
 from Pbs import SshPbs
 from CommandContainers.Modules import Modules
 from CommandContainers.ShellCommands import ShellCommands
+from CommandContainers.PbsCommands import PbsCommands
